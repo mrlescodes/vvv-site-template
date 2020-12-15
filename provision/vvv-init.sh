b81aa10a66748379ef651710180979b107e4760e
Les Aubrey <hello@mrles.co.uk>
2020-12-15 12:24:02 +0700

    Fix: Update conditional check for installing starter kit

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -40,8 +40,10 @@ setup_nginx_folders() {
 }
 
 install_starter_kit() {
+  echo " * install_starter_kit()"
+
   INSTALL_STARTER_KIT=$(get_config_value 'install_starter_kit' '')
-  if [[ ! -z "${INSTALL_STARTER_KIT}" ]] && [[ ! -d "${PUBLIC_DIR_PATH}/.git" ]]; then
+  if [ ! -z "${INSTALL_STARTER_KIT}" ] && [ ! -d "${PUBLIC_DIR_PATH}/.git" ]; then
     cd "${PUBLIC_DIR_PATH}"
 
     echo " * Installing starter kit"
